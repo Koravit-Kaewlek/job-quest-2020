139858f301cd86859a042ff22350d1d47da4be68
Koravit Kaewlek <koravit258@gmail.com>
2020-10-06 23:32:49 +0700

    create ex2 array-shift

--- basic-js-ts/array-shift.js
+++ basic-js-ts/array-shift.js
@@ -1,1 +1,21 @@
+function shift(arr = [], direction, num) {
+  const len = arr.length;
+  let result = [];
+  switch (direction) {
+    case 'left':
+      const left = arr.splice(0, num % len);
+      result = [...arr, ...left];
+      break;
+    case 'right':
+      const right = arr.splice(len - (num % len), len);
+      result = [...right, ...arr];
+      break;
+    default:
+      result;
+  }
+
+  return result;
+}
 console.log('----- Ex.2 Array Shift -----');
+console.log(shift(['john', 'jane', 'sarah', 'alex'], 'left', 2));
+console.log(shift([1, 2, 3, 4, 5], 'right', 3));
